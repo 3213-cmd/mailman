d00da6a66d6cf8fcc29c8fa4522ff6f53e7fabc8
Nikolas Sibaev <nikolas@sibaev.de>
2023-09-16 17:08:06 +0200

    added lacinia/graphql to project

---
+++ src/mailman/back/db/graphql.clj
@@ -0,0 +1,32 @@
+(ns mailman.back.db.graphql
+  (:require [mailman.back.db.schema :as s]
+            [com.walmartlabs.lacinia :as lacinia]
+            [clojure.walk :as walk])
+  (:import (clojure.lang IPersistentMap)))
+
+(def schema (s/load-schema))
+
+(defn simplify
+  "Converts all ordered maps nested within the map into standard hash maps, and
+   sequences into vectors, which makes for easier constants in the tests, and eliminates ordering problems."
+  [m]
+  (walk/postwalk
+    (fn [node]
+      (cond
+        (instance? IPersistentMap node)
+        (into {} node)
+
+        (seq? node)
+        (vec node)
+
+        :else
+        node))
+    m))
+
+(defn q
+  [query-string]
+(-> (lacinia/execute schema query-string nil nil)
+      simplify))
+
+;; (q "{ account(id: 1) { id name }}" )
+;; (q "{ service(id: 2) { id domain }}" )
---
+++ src/mailman/back/db/schema.clj
@@ -0,0 +1,46 @@
+(ns mailman.back.db.schema
+ "Contains custom resolvers and a function to provide the full schema."
+  (:require [clojure.java.io :as io]
+            [com.walmartlabs.lacinia.util :as util]
+            [com.walmartlabs.lacinia.schema :as schema]
+            [clojure.edn :as edn]
+            [next.jdbc :as jdbc]
+            [clojure.set :as set]
+            [mailman.back.db.queries :as queries]))
+
+(defn- remap-account
+  [row-data]
+  (set/rename-keys row-data {:accounts/id     :id
+                             :accounts/name   :name}))
+
+(defn- remap-service
+  [row-data]
+  (set/rename-keys row-data {:service_information/id     :id
+                             :service_information/name   :name
+                             :service_information/domain :domain
+                             :service_information/category :category
+                             :service_information/change_link :change_link
+                             :service_information/deletion_link :deletion_link
+                             }))
+
+(defn account-by-id
+  []
+  (fn [_ args _]
+    (remap-account (queries/find-account-by-id (:id args)))))
+
+(defn service-by-id
+  []
+  (fn [_ args _]
+    (remap-service (queries/find-service-by-id (:id args)))))
+
+(defn resolver-map []
+  {:Query/account (account-by-id)
+   :Query/service (service-by-id)})
+
+(defn load-schema
+  []
+  (-> (io/resource "./schemas/schema.edn")
+      slurp
+      edn/read-string
+      (util/inject-resolvers (resolver-map))
+      schema/compile))
