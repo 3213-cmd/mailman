4fb0d863ae0ec618a8ccda5cdef260d632f0aa81
Nikolas Sibaev <nikolas@sibaev.de>
2023-09-16 17:08:06 +0200

    Remove unused code

--- src/mailman/back/db/db.clj
+++ src/mailman/back/db/db.clj
@@ -176,28 +176,4 @@
     (create-tables))
   )
 
-;; (start-db)
-
-;; (defn get-all-users
-;;   []
-;;   (jdbc/execute!
-;;    db-source
-;;    ["create table Users (id int auto_increment primary key,
-;;    name varchar (255), email varchar (255))"])
-
-;;   (jdbc/execute!
-;;    db-source
-;;    ["insert into Users (name, email) values
-;;      ('The Doctor', 'timelord3000@tardis.com')"])
-
-;;   (jdbc/execute!
-;;    db-source
-;;    ["select * from users"])
-
-;;   (jdbc/execute!
-;;    db-source
-;;    ["select * from users"]
-;;    {:builder-fn rs/as-unqualified-lower-maps})
-;;   )
-
-;; (get-all-users)
+ ;; (start-db)
