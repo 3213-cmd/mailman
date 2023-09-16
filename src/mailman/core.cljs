8d031bb7c07c8482fcee1236ead1b96fca467105
Nikolas Sibaev <nikolas@sibaev.de>
2023-09-16 17:08:05 +0200

    Splitting up the views

--- src/mailman/core.cljs
+++ src/mailman/core.cljs
@@ -11,6 +11,8 @@
    [spec-tools.data-spec :as ds]
    [fipp.edn :as fedn]
    [reagent-mui.material.button :refer [button]]
+   [reagent-mui.material.icon-button :refer [icon-button]]
+   [reagent-mui.icons.airport-shuttle :refer  [airport-shuttle] ]
    [reagent-mui.material.app-bar :refer [app-bar]]
    [reagent-mui.material.toolbar :refer [toolbar]]
    [reagent-mui.material.typography :refer [typography]]
@@ -19,81 +21,31 @@
    [reagent-mui.material.container  :refer [container]]
    [reagent-mui.material.css-baseline :refer [css-baseline]]
    [reagent-mui.styles :as styles]
+   [mailman.mail-list.views]
+   [mailman.general.views]
    ))
 
 
 ;; WORKS with dev but not npm, check dev.cljs.edn and npm.cljs.edn
 
-(def columns [{:field      :id
-               :headerName "ID"
-               :width      90}
-              {:field      :first-name
-               :headerName "First name"
-               :width      150
-               :editable   true}
-              {:field      :last-name
-               :headerName "Last name"
-               :width      150
-               :editable   true}
-              {:field      :age
-               :headerName "Age"
-               :type       :number
-               :width      110
-               :editable   true}
-              {:field       :full-name
-               :headerName  "Full name"
-               :description "This column has a value getter and is not sortable."
-               :sortable    false
-               :width       160
-               :valueGetter (wrap-clj-function
-                             (fn [params]
-                               (str (get-in params [:row :first-name] "") " " (get-in params [:row :last-name] ""))))}])
-
-(def rows [{:id 1 :last-name "Snow" :first-name "Jonat" :age 35}
-           {:id 2 :last-name "Lannister" :first-name "Cersei" :age 42}
-           {:id 3 :last-name "Lannister" :first-name "Jaime" :age 45}
-           {:id 4 :last-name "Stark" :first-name "Arya" :age 16}
-           {:id 5 :last-name "Targaryen" :first-name "Daenerys" :age nil}
-           {:id 6 :last-name "Melisandre" :first-name nil :age 150}
-           {:id 7 :last-name "Clifford" :first-name "Ferrara" :age 44}
-           {:id 8 :last-name "Frances" :first-name "Rossini" :age 36}
-           {:id 9 :last-name "Roxie" :first-name "Harvey" :age 65}])
-
-(defn component []
-  [:div {:style {:height 400 :width 800}}
-   [data-grid {:rows                           rows
-               :columns                        columns
-               :initial-state                  (clj->js' {:pagination {:pagination-model {:page-size 5}}})
-               :page-size-options              [5]
-               :checkbox-selection             true
-               :disable-row-selection-on-click true}]])
 
 
 
-(defn home-page []
-  [:div [:h2 "Hello I am at home!"]
-   [:li [:a {:href "#/about"} "Homepage"]]
-   ])
 
 
-(defn about-page []
-  [:div
-   [:h2 "Hello I am at work!"]
-   [:li [:a {:href (rfe/href ::homepage)} "Workpage"]]
-   ])
 
 
 (def routes
   [["/home"
     {:name ::homepage
-     :view home-page}]
+     :view mailman.general.views/home-page}]
 
    ["/about"
     {:name ::aboutpage
-     :view about-page}]
-   ["/grid"
-    {:name ::grid
-     :view component}]
+     :view mailman.general.views/about-page}]
+   ["/mail-list"
+    {:name ::mail-list
+     :view mailman.mail-list.views/mail-list}]
    ])
 
 
@@ -127,18 +79,23 @@
       {:variant "contained"
        :size "small"
        ;; :on-click #(js/alert "Hello there!")
+       :href (rfe/href ::homepage)
        }
-      [:a {:href (rfe/href ::aboutpage)} "Frontpage"]]
+      "Home"]
      [button
       {:variant "contained"
        :size "small"
-       }[:a {:href (rfe/href ::homepage)} "Workpage"] ]
+       :href (rfe/href ::aboutpage)
+       } "About" ]
      [button
       {:variant "contained"
        :size "small"
+       :href (rfe/href ::mail-list)
        }
-      [:a {:href (rfe/href ::grid)} "Grid"]
-      ]]]
+      "Mail List"
+      ]
+     [button {:variant "contained" } "Airports"]
+     ]]
    (if @match
      (let [view (:view (:data @match))]
        [view @match]))
