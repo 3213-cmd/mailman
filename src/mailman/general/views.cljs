aa9fce1c027ea872569518472da9c08df609afbf
Nikolas Sibaev <nikolas@sibaev.de>
2023-09-16 17:08:05 +0200

    Added Select item

--- src/mailman/general/views.cljs
+++ src/mailman/general/views.cljs
@@ -1,19 +1,46 @@
 (ns mailman.general.views
   (:require
    [reagent-mui.material.button :refer [button]]
+   [reagent-mui.material.select :refer [select]]
+   [reagent-mui.material.form-control :refer [form-control]]
+   [reagent-mui.material.input-label :refer [input-label]]
+   [reagent-mui.material.menu-item :refer [menu-item]]
    [reagent-mui.material.text-field :refer [text-field]]
    [reagent-mui.material.stack :refer [stack]]
    [reagent-mui.material.grid :refer [grid]]
    [reagent-mui.material.container :refer [container]]
    [reagent.core :as reagent :refer [atom]]
    [reagent-mui.x.date-picker :refer [date-picker]]
+   [reagent-mui.material.tooltip :refer [tooltip]]
    ;; [reagent-mui.x.date-range-picker-pro :refer [date-range-picker-pro]]
    ))
 (defonce date-picker-state (reagent/atom nil))
 (defonce text-state (reagent/atom "foobar"))
 (defonce select-state (reagent/atom 1))
+(defonce email-provider (reagent/atom "Google"))
 (defn event-value [e]
   (.. e -target -value))
+
+(defn email-provider-selector []
+  [grid {:item true :xs 12 :sm 6}
+   [form-control {:autoWidth true }
+    [input-label {:id "email-provider-label"} "Provider"]
+    [select {:label "Provider"
+             :labelid "email-provider-label"
+             :value @email-provider
+             :on-change (fn [e]
+                          (reset! email-provider (event-value e)))}
+
+     [menu-item {:value "Google"} "Google"]
+     [menu-item {:value "GMX"} "GMX"]
+     [menu-item {:value "Hotmail"} "Hotmail"]
+     [menu-item {:value "Custom"} "Custom"]
+     ]]]
+  )
+(defn custom-email-provider []
+  (if (== (compare "Custom" @email-provider) 0)
+    [:h2 "IMAP Settings"
+     ] nil))
 
 (defn home-page []
   [container {:component "main" :max-width "xs"}
@@ -25,15 +52,18 @@
     [grid
      {:container true
       :spacing   2}
-     [grid {:item  true :xs 12 :sm 6}
-      [text-field
-       {:value       @text-state
-        :label       "Mailprovide1"
-        :placeholder "Placeholder"
-        :helper-text "Helper text"
-        :width 200
-        :on-change   (fn [e]
-                       (reset! text-state (event-value e)))}]]
+     [tooltip {:title "The name for this connection"}
+      [grid {:item  true :xs 12 :sm 6}
+       [text-field
+        {:value       @text-state
+         :label       "Name"
+         :placeholder "Placeholder"
+         :helper-text "Helper text"
+         :width 200
+         :on-change   (fn [e]
+                        (reset! text-state (event-value e)))}]]]
+     (email-provider-selector)
+     (custom-email-provider)
      [grid {:item  true :xs 12 :sm 6}
       [text-field
        {:value       @text-state
