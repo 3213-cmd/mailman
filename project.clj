39c79a702feb6696c64999467657f7fd6face002
Nikolas Sibaev <nikolas@sibaev.de>
2023-09-16 17:08:06 +0200

    Create shadow-cljs.edn, change project.clj
    - Added Backend to the project, now managed by project.clj
    - Frontend is now handled by shadow-cljs.edn

--- project.clj
+++ project.clj
@@ -7,28 +7,36 @@
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/clojurescript "1.11.60"]
-                 [arttuka/reagent-material-ui "5.11.12-0" :exclusions [arttuka/reagent-material-ui-js]]
-                 [reagent "1.2.0"  :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
-                 [metosin/reitit "0.7.0-alpha5"]
-                 [cljs-ajax "0.8.4"]
+                 [io.forward/clojure-mail "1.0.8"]
+                 [com.github.seancorfield/next.jdbc "1.3.883"]
+                 [com.github.seancorfield/honeysql "2.4.1045"]
+                 [org.xerial/sqlite-jdbc "3.42.0.0"]
+                 [org.clojure/data.csv "1.0.1"]
+                 [com.google.guava/guava  "32.1.2-jre"]
+                 [dev.weavejester/medley "1.7.0"]
                  ]
 
   :source-paths ["src" "target"]
 
-  :aliases {"fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
-            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
-            "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "mailman.test-runner"]
-            "run:dev" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
-            "run:npm" ["trampoline" "run" "-m" "figwheel.main" "-b" "npm" "-r"]}
+
+
+
+  ;; :aliases {
+  ;;           "fig:build2" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev"]
+  ;;           "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+  ;;           "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
+  ;;           "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "mailman.test-runner"]
+  ;;           "run:dev" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+  ;;           "run:npm" ["trampoline" "run" "-m" "figwheel.main" "-b" "npm" "-r"]}
 
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.18"]
                                   [org.slf4j/slf4j-nop "2.0.7"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
                    :clean-targets ^{:protect false} [:target-path "resources/public/cljs-out" "target" "resources/public/js" ".shadow-cljs"   ]}
-             :npm {:dependencies [[com.bhauman/figwheel-main "0.2.18"]
-                                  [org.slf4j/slf4j-nop "2.0.7"]
-                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]
+             :npm {:dependencies 
+                   [[com.bhauman/figwheel-main "0.2.18"]
+                    [org.slf4j/slf4j-nop "2.0.7"]
+                    [com.bhauman/rebel-readline-cljs "0.1.4"]]
                    :clean-targets ^{:protect false} [:target-path "resources/public/cljs-out" "target" "resources/public/js" ".shadow-cljs"   ]}
              })
 ;; check clean targets meaning and compare both:
